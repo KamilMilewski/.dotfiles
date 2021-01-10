fb1dd6288cbdc1d545d7c6672f58217a3d4fde65
Kamil Milewski <kamil.milewski@hotmail.com>
2021-01-10 09:27:49 +0000

    save point

--- system_workspaces/tap.sh
+++ system_workspaces/tap.sh
@@ -1,13 +1,13 @@
-code &&
-(xfce4-terminal -e 'tmuxp load tap' &> /dev/null &) &&
-wmctrl -s 3 &&
-(chromium --new-window https://track.toggl.com/timer &> /dev/null &) &&
 (slack &> /dev/null &) &&
-sleep 4 &&
-wmctrl -r Code -t 0 &&
-wmctrl -r Terminal -t 1 &&
-wmctrl -r Code -b add,maximized_vert,maximized_horz &&
+(chromium --new-window https://track.toggl.com/timer &> /dev/null &) &&
+(xfce4-terminal -e 'tmuxp load tap' &> /dev/null &) &&
+sleep 1 &&
 wmctrl -r Terminal -b add,maximized_vert,maximized_horz &&
+sleep 10 &&
 wmctrl -r Toggl -b add,maximized_vert,maximized_horz &&
 wmctrl -r Slack -b add,maximized_vert,maximized_horz &&
+wmctrl -r Slack -t 1 &&
 disown
+
+# Notes:
+# move to workspace: wmctrl -s 3 &&
