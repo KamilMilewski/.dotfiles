48095e0e3ea15a1f212e1c6178b22b766e9a43b6
Kamil Milewski <kamil.milewski@hotmail.com>
2021-03-17 09:30:33 +0000

    save point

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -11,14 +11,14 @@ map('i', 'jj', '<esc>', { noremap = true })
 map('n', 'j', 'gj', { noremap = true })
 map('n', 'k', 'gk', { noremap = true })
 
--- Remap `leave insert mode` to ctrl+s in terminal
-map('i', '<C-s>', '<ESC>', { noremap = true })
-
 -- Map copy to system clipboard
 map('v', '<leader>y', '"+y', { noremap = true })
 
--- Map leave insert mode in vim terminal to ctrl+s
+-- Map `leave insert mode` in vim terminal to ctrl+s
 map('t', '<C-s>', '<C-\\><C-n>', { noremap = true })
+
+-- Map `leave insert mode` to ctrl+s
+map('i', '<C-s>', '<ESC>', { noremap = true })
 
 -- Quick exit
 map('n', '<leader>qq', '<cmd>:qa!<CR>', { noremap = true })
