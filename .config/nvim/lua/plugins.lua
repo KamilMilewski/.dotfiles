051ebf264ad13cc83c37229adddb28c48d353785
Kamil Milewski <kamil.milewski@hotmail.com>
2024-02-06 23:03:27 +0100

    save point

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -141,6 +141,12 @@ local plugins = {
       vim.cmd [[
 	" Trigger a highlight in the appropriate direction when pressing these keys:
 	let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
+
+	augroup qs_colors
+	  autocmd!
+	  autocmd ColorScheme * highlight QuickScopePrimary guifg='#99CE64' gui=underline ctermfg=155 cterm=underline
+	  autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+	augroup END
       ]]
   },
 
