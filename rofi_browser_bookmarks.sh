12ff342359223543bf5118da49b08a7db52d2002
Kamil Milewski <kamil.milewski@hotmail.com>
2024-02-14 21:03:40 +0100

    save point

--- rofi_browser_bookmarks.sh
+++ rofi_browser_bookmarks.sh
@@ -2,11 +2,19 @@
 # How it works:
 if [[ -z "$@" ]]; then
   # Here we are preparing list of items (bookmark URLs in this case) that will be displayed in Rofi menu.
-  # '.. | .url? | strings' - is for recursively searching for values under `url` key
-  cat  ~/.config/google-chrome/Default/Bookmarks | jq -cr '.. | .url? | strings'
+  # `.. | [.name?,.url?]`  - recursively search for values under name and url keys. This will produce an array like:
+  # [{name: 'some name', url: 'some url'}, ...]
+  # `select( . != [])`     - filter out empty arrays (on some levels in a JSON there is no url nor name keys)
+  # `select(.[1] != null)` - filter out entries that have second element (url) in an array as null (those are bookmark directories names)
+  # `@tsv`  	           - concat every array element into one string. This will produce an array of strings like (bookmark name www.bookmark.url.com)
+  cat ~/.config/google-chrome/Default/Bookmarks | jq -cr '.. | [.name?,.url?] | select( . != [] ) | select(.[1] != null) | @tsv'
 else
   # When some of the options (URL) gets selected Rofi again calls the script with option as an argument and this branch
   # gets executed.
-  zsh -c "google-chrome-stable '$@' &> /dev/null &"
+  # example of $@: `Online Rain Sound Generator and Forecast | Rain.today   https://rain.today/`
+  # So, its a bookmark name and an actual url at the end
+  #
+  URL=$(echo $@ | awk '{print $NF}') # extract last element of a string which is an url
+  zsh -c "google-chrome-stable '${URL}' &> /dev/null &"
 fi
 
