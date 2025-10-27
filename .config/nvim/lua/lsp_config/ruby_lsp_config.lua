7b66f2489886b07b1c45af356c28854981e9e9fc
Kamil Milewski <kamil.milewski@hotmail.com>
2025-10-27 23:12:47 +0100

    save point

--- .config/nvim/lua/lsp_config/ruby_lsp_config.lua
+++ .config/nvim/lua/lsp_config/ruby_lsp_config.lua
@@ -1,5 +1,6 @@
 local LspSharedConfig = require("lsp_config/lsp_shared_config")
 vim.lsp.config('ruby_lsp', {
   on_attach = LspSharedConfig.on_attach,
+  root_markers = { "Gemfile.lock", "Gemfile", ".git" }
 })
 vim.lsp.enable("ruby_lsp")
